9dff08b96048f3f4784f3bb97df781f868751a7f
VEDANT KASHYAP <smartvedi@gmail.com>
2017-05-11 20:52:32 +0530

    Added the required Jquery and Twitter JS files ... wrote scripts for getting a random quote from a random quote generator API and for tweeting a quote.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 /**
  * Created by VEDANT KASHYAP on 5/11/2017.
  */
+
+var BaseURL = "https://twitter.com/intent/tweet?text=";
+
+function tweetText() {
+    $("a[href^='https://twitter.com/intent/tweet?text=']").prop("href", BaseURL + $("#quote").text().trim() + "%0D%0A" + $("#author-container").text().trim());
+}
+
+function getNewQuote() {
+    $("#quote").fadeOut(500, function () {
+        $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?",
+            function (getQuote) {
+                $("#quote").html(getQuote[0].content);
+                $("#author-container").text("- " + getQuote[0].title);
+                $("#quote").fadeIn(700);
+                $("#author-container").fadeIn(700);
+                tweetText();
+            })
+    })
+}
